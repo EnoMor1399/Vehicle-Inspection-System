1bd087551e8443fa4603847bba992ce0e0d76ab0
EnoMor1399 <morganenoch1@gmail.com>
2026-09-21 23:06:08 +0000

    Allow privileged users to reach mandatory 2FA enrollment

--- src/app/security/setup-2fa/page.tsx
+++ src/app/security/setup-2fa/page.tsx
@@ -2,6 +2,6 @@ import { requireAuth } from "@/lib/require-auth";
 import { Setup2FA } from "./Setup2FA";
 
 export default async function Setup2FAPage() {
-  await requireAuth();
+  await requireAuth({ allowPendingPrivileged2FA: true });
   return <Setup2FA />;
 }
